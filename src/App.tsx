bd980de23e7a355f8a7321c243ea796f39b241cc
Ashif Ahamad <ashif_ahamad@outlook.com>
2025-05-04 14:50:10 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import IncidentDashboard from './components/IncidentDashboard';
-import { ThemeProvider } from './context/ThemeContext';
 import './App.css';
 
 function App() {
   return (
-    <ThemeProvider>
+    
       <div className="App">
         <IncidentDashboard />
       </div>
-    </ThemeProvider>
+  
   );
 }
 
